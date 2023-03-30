237cadd71c7d3efa947a7d6467634b4696471c1c
Nikola <keha39@yandex.ru>
2023-03-30 12:55:09 +0300

    Добавьте файлы проекта.

---
+++ EpicrisisWord/Shared/Navigations/NavigationMessage.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace EpicrisisWord.Shared.Navigations
+{
+    internal class NavigationMessage : ValueChangedMessage<string>
+    {
+        internal NavigationMessage(string value) : base(value) { }
+    }
+}
