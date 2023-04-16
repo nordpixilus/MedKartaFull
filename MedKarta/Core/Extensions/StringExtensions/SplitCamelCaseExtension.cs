a1e837ed290fd0fa02fca71091c4774072f4ad73
Nikola <keha39@yandex.ru>
2023-04-16 12:33:51 +0300

    создание расширения

---
+++ MedKarta/Core/Extensions/StringExtensions/SplitCamelCaseExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MedKarta.Core.Extensions.StringExtensions;
+
+internal static class SplitCamelCaseExtension
+{
+    internal static string SplitCamelCase(this string nameClass)
+    {
+        if (nameClass != null)
+        {
+            string[] split = Regex.Split(nameClass, @"(?<!^)(?=[A-Z])");
+            return split[1];
+        }
+
+        return  "";
+    }
+}
