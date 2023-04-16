725dca2cc9f833468aae78eca9d07304fad694a1
Nikola <keha39@yandex.ru>
2023-04-17 00:00:26 +0300

    подключение моделей

--- MedKarta/Core/Extensions/DependencyInjection/AddViewExtensions.cs
+++ MedKarta/Core/Extensions/DependencyInjection/AddViewExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MedKarta.Core.Extensions.StringExtensions;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace MedKarta.Core.Extensions.DependencyInjection
@@ -10,9 +11,13 @@ namespace MedKarta.Core.Extensions.DependencyInjection
             var viewType = typeof(TView);
             var viewTypeNamespace = viewType.Namespace;
             string viewTypeFullName = viewType.Name;
-            //Type modelType = Type.GetType(string.Format("{0}.{1}", viewTypeNamespace, "Model"))!;
+
+            string? nameBase = viewTypeFullName.SplitPascalCase();            
+            Type? modelType = Type.GetType(string.Format("{0}.{1}", viewTypeNamespace, $"{nameBase}Model"));
+            if (modelType != null ) services.AddSingleton(modelType);
+
             Type viewModelType = Type.GetType(string.Format("{0}.{1}", viewTypeNamespace, $"{viewTypeFullName}Model"))!;
-            //services.AddSingleton(modelType);
+            
             services.AddSingleton(viewModelType);
             services.AddSingleton(typeof(TView));
 
