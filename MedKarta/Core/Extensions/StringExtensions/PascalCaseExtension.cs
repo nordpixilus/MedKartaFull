a10e9899b476da36605d11595bd1fbcd06d57d28
Nikola <keha39@yandex.ru>
2023-04-16 20:39:08 +0300

    первый тест

---
+++ MedKarta/Core/Extensions/StringExtensions/PascalCaseExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MedKarta.Core.Extensions.StringExtensions;
+
+public static partial class PascalCaseExtension
+{
+    public static bool IsPascalCase(this string? strPascalCase)
+    {
+        if (string.IsNullOrWhiteSpace(strPascalCase)) return false;
+
+        if (!MyRegex().IsMatch(strPascalCase)) return false;
+
+        if (!Char.IsUpper(strPascalCase, 0)) return false;
+
+        if(strPascalCase.Count(m => char.IsUpper(m)) == 1) return false;    
+
+        return true;
+    }
+
+    public static string SplitPascalCase(this string? nameClass)
+    {
+        if (nameClass.IsPascalCase())
+        {
+            ArgumentException.ThrowIfNullOrEmpty("Строка не соответсвует названию класса", nameof(nameClass));
+        }
+
+        string[] split = Regex.Split(nameClass!, @"(?<!^)(?=[A-Z])");
+        return split[1];
+    }
+
+    [GeneratedRegex("^[a-zA-Z]+$")]
+    private static partial Regex MyRegex();
+}
---
+++ MedKartaTest/Core/Extensions/StringExtensions/IsPascalCaseTest.cs
@@ -0,0 +1,31 @@
+﻿using MedKarta.Core.Extensions.StringExtensions;
+
+namespace MedKartaTest.Core.Extensions.StringExtensions;
+
+[TestClass]
+public class IsPascalCaseTest
+{
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    [DataRow("   ")]
+    [DataRow("Gg9")]
+    [DataRow("Тест")]
+    [DataRow("jkhkhkjh")]
+    [DataRow("view Model")]
+    [DataRow("viewModel")]
+    [DataRow("Model")]
+    public void IsPascalCaseTest_NotNormal_error(string? testStr)
+    {
+        Assert.IsFalse(testStr.IsPascalCase());
+    }
+
+    [DataTestMethod]
+    [DataRow("ViewModel")]
+    public void IsPascalCaseTest_Normal_true(string testStr)
+    {
+        Assert.IsTrue(testStr.IsPascalCase());
+    }
+}
