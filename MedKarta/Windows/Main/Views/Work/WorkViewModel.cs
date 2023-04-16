4b3423d0c17ad063021e7fe55b36b92e524df0a6
Nikola <keha39@yandex.ru>
2023-04-16 08:14:54 +0300

    проверка

--- MedKarta/Windows/Main/Views/Work/WorkViewModel.cs
+++ MedKarta/Windows/Main/Views/Work/WorkViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using MedKarta.Core.Models;
 using MedKarta.Shared.Navigations;
 using MedKarta.Windows.Main.Views.Start;
 
 namespace MedKarta.Windows.Main.Views.Work
 {
-    internal partial class WorkViewModel : ObservableRecipient
+    internal partial class WorkViewModel : BaseViewModel
     {
         public string TextLabel { get; set; } = "WorkView";
         public string TextButton { get; set; } = "GoToHomeView";
