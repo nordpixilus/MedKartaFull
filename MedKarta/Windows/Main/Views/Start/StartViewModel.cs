87773459856150c96d65672eb6a47de6155fe18f
Nikola <keha39@yandex.ru>
2023-04-18 23:09:46 +0300

    начальный вывод данных в таблицу не доделанных

--- MedKarta/Windows/Main/Views/Start/StartViewModel.cs
+++ MedKarta/Windows/Main/Views/Start/StartViewModel.cs
@@ -1,5 +1,28 @@
-﻿using MedKarta.Core.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MedKarta.Core.Models;
+using MedKarta.Entity.Context;
+using MedKarta.Entity.Table.User;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace MedKarta.Windows.Main.Views.Start;
 
-internal partial class StartViewModel : BaseViewModel { }
+internal partial class StartViewModel : BaseViewModel
+{
+    //[ObservableProperty]
+    //private List<Person> _People;
+    //public ObservableCollection<Person>? People { get; }
+    public StartViewModel()
+    {
+        //medKartaContext.Persons.Load();
+        //_People = medKartaContext.Persons.Local.ToObservableCollection();
+
+        //_People = medKartaContext.Persons
+        //    .Where(p => p.StepId == 2)
+        //    //.Select(p => new { p.Kod, p.FullName })
+        //    .ToList()
+        //    ;
+    }
+}
